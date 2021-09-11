b4b233f0a0646608b8f8d3d5c3571e6a9301ff1f
Larisse Rodrigues <57159487+helloLari@users.noreply.github.com>
2021-09-11 02:10:34 -0300

    mongodb

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const MONGO_URL=process.env.MONGODB_URI || 'mongodb://localhost:27017/ProjetoFinal'   
+const MONGO_URL=process.env.MONGODB_URI || 'mongodb://localhost:27017/Projet0'   
 
 
 const connect=()=>
